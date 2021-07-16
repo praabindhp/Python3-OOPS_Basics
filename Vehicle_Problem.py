82cfe11b5cecd90e8b62ee8e2138e0db850dc00b
praabindhp <praabindhp@gmail.com>
2021-07-16 22:04:22 +0530

    OOPS Vehicle Problem Solution

---
+++ Vehicle_Problem.py
@@ -0,0 +1,33 @@
+class Vehicle:
+     
+    def __init__(self, brand, year):
+        self.brand = brand
+        self.year = year
+ 
+    def fuel_consumption(self):
+        pass
+ 
+ 
+class Bus(Vehicle):
+ 
+    def __init__(self, brand, year):
+        super().__init__(brand, year)
+ 
+    def fuel_consumption(self):
+        print('Fuel Consumption For Bus...')
+ 
+ 
+class Car(Vehicle):
+ 
+    def __init__(self, brand, year):
+        super().__init__(brand, year)
+ 
+    def fuel_consumption(self):
+        print('Fuel Consumption For Car...')
+ 
+ 
+bus = Bus(brand='Ford', year=1957)
+bus.fuel_consumption()
+ 
+car = Car(brand='BMW', year=1998)
+car.fuel_consumption()

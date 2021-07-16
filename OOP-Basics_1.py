2b26abd29180c17b766b5aa87c0d766213b9c184
praabindhp <praabindhp@gmail.com>
2021-07-16 21:15:59 +0530

    OOPS Basic 1

---
+++ OOP-Basics_1.py
@@ -0,0 +1,21 @@
+class person:
+
+    # name = ['Kevin', 'Henry']
+    name = 'Praabindh'
+    age = 20
+    # name = [24, 25]
+
+    def __init__(self, name = 'Praabindh', age = 24):
+        self.name = name
+        self.age = age
+
+    def show_name(self):
+        # print('Name : ' + self.name)
+        # print('Age : ' + self.age)
+        print('Name : %s \nAge : %s' % (self.name, self.age))
+
+# p = person(person.name, person.age)
+
+p = person(name = 'Sam')
+p = person(age = 20)
+p.show_name()

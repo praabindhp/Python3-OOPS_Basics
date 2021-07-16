2bdc1fbf1b03c990960609c7f0b0145146ec3db6
praabindhp <praabindhp@gmail.com>
2021-07-16 21:40:43 +0530

    OOPS Basic Two

---
+++ OOP-Basics_2.py
@@ -0,0 +1,20 @@
+# Super Keyword
+
+class User:
+
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
+
+    def show_user(self):
+        print("The User : %s \nThe Age : %s" % (self.name, self.age))
+
+class FacebookUser(User):
+    
+    def __init__(self, name, age):
+        # self.name = name
+        # self.age = age
+        super().__init__(name, age)
+
+FB_User = FacebookUser('Praabindh Pradeep', 20)
+FB_User.show_user()
